c0789c89a28b144689fac8dd564169f8616c02cc
masoudjaliliaziz <masoudjalili0781@gmail.com>
2025-01-22 00:06:12 +0330

    using the props of styled components for conditional rendering of any styled component by passing thde as props for also give us the right semantic html tag

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
+import Heading from "./ui/Headind";
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
@@ -13,6 +14,10 @@ const H1 = styled.h1`
 
 const StyledApp = styled.div`
   background-color: orangered;
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+  width: 50%;
   padding: 20px;
 `;
 function App() {
@@ -20,9 +25,12 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>hello mother fucker</H1>
+        <Heading as={`h1`}>the wild oaisis</Heading>
+
+        <Heading as={`h2`}>check in & check out</Heading>
         <Button>check in</Button>
         <Button>check out</Button>
+        <Heading as={`h3`}>forms</Heading>
         <Input type="number" placeholder="Number of guests" />
       </StyledApp>
     </>
