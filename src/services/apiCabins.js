6546a7f62d987b2fce80a62d91553d3faacc3937
masoudjaliliaziz <masoudjalili0781@gmail.com>
2025-01-24 14:24:06 +0330

    adding react hook form to add the cabins and use it in the form

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -17,3 +17,15 @@ export async function deleteCabin(id) {
     throw new Error("cabins could not be deleted");
   }
 }
+
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw new Error("cabins could not be created");
+  }
+}
