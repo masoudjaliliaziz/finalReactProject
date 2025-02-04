e11394c91917666ee36577fe3f77a5c66ddff2a8
masoudjaliliaziz <masoudjalili0781@gmail.com>
2025-02-04 18:22:28 +0330

    adding prefetching for pagination in booking

--- src/features/bookings/hooks/useBookings.js
+++ src/features/bookings/hooks/useBookings.js
@@ -1,9 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 import { getBookings } from "../../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../../utils/constants";
 
 export function useBookings() {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
   //FILTER-------------
   const filteredValue = searchParams.get("status");
@@ -32,5 +34,20 @@ export function useBookings() {
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
+  //PREFETCHING--------------
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+  if (page < pageCount) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+  }
+
+  if (page > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
+  }
   return { isLoading, bookings, error, count };
 }
