139450e36aff303f9683ca687403bece40db765b
masoudjaliliaziz <masoudjalili0781@gmail.com>
2025-02-18 12:20:09 +0330

    fix bug for navigate to dashboard when user log in

--- src/features/authentication/hook/useLogin.js
+++ src/features/authentication/hook/useLogin.js
@@ -1,16 +1,17 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../../services/apiAuth";
-import { useNavigate } from "react-router-dom";
+import { replace, useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
 
 export function useLogin() {
-  const queryClient = useQueryClient();
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      navigate("/dashboard");
-      queryClient.setQueryData(["user"], user);
+      navigate("/dashboard", { replace: true });
+      queryClient.setQueryData(["user"], user.user);
     },
     onError: (err) => {
       console.log("ERROR", err);
