f6fa8a27df2e19141d6123075222dea73d95b5da
masoudjaliliaziz <masoudjalili0781@gmail.com>
2025-02-19 17:49:54 +0330

    adding pie chart for stay duration summary in dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import { useRecentStays } from "./hooks/useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "./../cabins/hooks/useCabins";
 import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
@@ -33,7 +34,7 @@ function DashboardLayout() {
         cabinCount={cabins.length}
       />
       <div>todays Activity</div>
-      <div>chart</div>
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
